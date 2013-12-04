5a01338e938e3461138dfb1b2525db07bfb71703
Hidayat Halim <misterpy@gmail.com>
2013-12-04 23:20:00 +0100

    Added pseudocode comments

--- src/firmware/main.c
+++ src/firmware/main.c
@@ -11,6 +11,17 @@ int main(void)
     /* insert your hardware initialization here */
     for(;;){
         /* insert your main loop code here */
+
+        //Read the data from sd card
+
+		//Write the data from sd card
+
+		//Read the data from GPS
+
+		//Calculate the distance of the coordinates
+
+		//Display the data in LCD
+
     }
     return 0;   /* never reached */
 }
