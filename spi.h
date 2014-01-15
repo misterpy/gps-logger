341f9a85cd7ea117ab1c840023cc4a4b0bbc361a
manue1 <manub1312@googlemail.com>
2014-01-15 10:27:49 +0100

    Cleaned code.

--- spi.h
+++ spi.h
@@ -1,14 +1,9 @@
 // SPI Communication protocol header
 
-<<<<<<< HEAD
 #ifndef SPI_H
 #define SPI_H
 
 #include "global.h"
-=======
-#ifndef _SPI_H_
-#define _SPI_H_
->>>>>>> af97042b507ae4b67adee6171f6223403292c78e
 
 #define SPI_SD             SPCR = 0x52
 #define SPI_HIGH_SPEED     SPCR = 0x50; SPSR |= (1<<SPI2X)
