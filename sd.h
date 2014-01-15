d5e17f662a48d163e6d883707de54d1560c30109
manue1 <manub1312@googlemail.com>
2014-01-15 10:27:29 +0100

    Deleted multiple block read/write. Not needed for FAT implementation.

--- sd.h
+++ sd.h
@@ -25,9 +25,7 @@
 #define SEND_STATUS              13
 #define SET_BLOCK_LEN            16
 #define READ_SINGLE_BLOCK        17
-#define READ_MULTIPLE_BLOCKS     18
 #define WRITE_SINGLE_BLOCK       24
-#define WRITE_MULTIPLE_BLOCKS    25
 #define ERASE_BLOCK_START_ADDR   32
 #define ERASE_BLOCK_END_ADDR     33
 #define ERASE_SELECTED_BLOCKS    38
@@ -39,6 +37,9 @@
 #define ON     1
 #define OFF    0
 
+#define TX_NEWLINE {transmitByte(0x0d); transmitByte(0x0a);} /*transferred from UART_sd.h*/
+
+
 volatile unsigned long startBlock, totalBlocks; 
 volatile unsigned char SDHC_flag, cardType, buffer[512];
 
@@ -46,8 +47,6 @@ unsigned char SD_init(void);
 unsigned char SD_sendCommand(unsigned char cmd, unsigned long arg);
 unsigned char SD_readSingleBlock(unsigned long startBlock);
 unsigned char SD_writeSingleBlock(unsigned long startBlock);
-unsigned char SD_readMultipleBlock (unsigned long startBlock, unsigned long totalBlocks);
-unsigned char SD_writeMultipleBlock(unsigned long startBlock, unsigned long totalBlocks);
 unsigned char SD_erase (unsigned long startBlock, unsigned long totalBlocks);
 
 #endif
