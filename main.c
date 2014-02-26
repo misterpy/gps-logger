30cb892178681e3a2e27622a1ab4c0d755947b83
Hidayat Halim <misterpy@gmail.com>
2014-02-26 21:11:47 +0100

    Changed the overall order of initialisation

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
+/*----------------------------------------------------------------------*/
+/* Foolproof FatFs sample project for AVR              (C)ChaN, 2013    */
+/*----------------------------------------------------------------------*/
+
 #include <avr/sleep.h>
 #include "global.h"
+#include "ff.h"		/* Declarations of FatFs API */
 #include "gps.h"
 #include "calc.h"
-#include "fat32.h"
-#include "sd.h"
-#include "spi.h"
 #include "lcd.h"
+
+FATFS FatFs;		/* FatFs work area needed for each volume */
+FIL Fil;			/* File object needed for each open file */
 
 // Set GPS Message update rate to 1 HZ using 9600 as Baudrate
 #define FREQUENCY 1
@@ -15,200 +20,196 @@
 
 // This array will store the actual value of NMEA message
 // receive from the GPS module.
-char nmeabuff[128];
+BYTE nmeabuff[128];
 
 // This array will store the initial value read from SD Card
-char sdbuff[128];
+BYTE sdbuff[28];
 
-/*
-    Value of Latitude, Longitude and distance will be stored here
-    for further used such as displaying on the LCD and storing
-    into the SD Card
-*/
-char pLatGPS[COORDINATE_BUFFER_SIZE];
-char pLongGPS[COORDINATE_BUFFER_SIZE];
+// Value of Latitude, Longitude and distance will be stored here
+// for further used such as displaying on the LCD and storing
+// into the SD Card
 
-char pLatSD[COORDINATE_BUFFER_SIZE];
-char pLongSD[COORDINATE_BUFFER_SIZE];
+char pLatGPS[20];
+char pLongGPS[20];
+
+char pLatSD[20];
+char pLongSD[20];
 
 char pDistance[20];
 
-// Initialize all devices
-void initDevices(void) {
-    
-    spi_init();
+int main (void)
+{
+	_delay_ms(3000);
+	
+	sei();
+	
+	int i, j, cur_pos_lat, cur_pos_lon, write_pos=0;
 
-    /* Hardware initialisation */
-    gps_init(FREQUENCY, MESSAGES);
+	UINT bw;
+	DDRC |= 0xFF;
 
-    /* LCD Initialisation */
-    lcd_init(LCD_DISP_ON_CURSOR);
+	//Hardware initialisation 
+	gps_init(FREQUENCY, MESSAGES);
 
-}
+	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+	
+	_delay_ms(1000);
 
+	f_mount(&FatFs, "", 0);		/* Give a work area to the default drive */
 
-// Main function for the whole software
-int main(void){
+	if (f_open(&Fil, "IN.txt", FA_READ | FA_OPEN_EXISTING) == FR_OK) {	/* Read the file from SD */
+			
+			f_read(&Fil, sdbuff, 28, &bw);
+			
+			f_close(&Fil);
+			
+			if(bw == 28){
+				PORTC |= 0xFF;
+			}
+		}
 
-    unsigned char error, i; //used to check for errors in initialization phases
+	/* Disable unwanted module */
+	PRR0 |= (1<<PRTWI) | (1<<PRTIM0) | (1<<PRTIM2) | (1<<PRTIM1) | (1<<PRADC);
 
-    _delay_ms(100);     //delay for VCC stabilization
-
-    // Activate interrupts
-    sei(); 
-
-    /* Port configuration */
-    // code goes here
-
-    /* Disabling unwanted modules */
-    // code goes here
-
-    initDevices();      // Call initialization function
-
-    /* move cursor to correct position for welcome message*/
-    lcd_clrscr();	// Clear screen of LCD
-    lcd_home();		// bring cursor to 0,0
-    lcd_puts ("--- GPS LOGGER ---");
-    lcd_gotoxy(0,1);	// Go to 2nd row 1st column
+	lcd_clrscr();
+    lcd_home();
+    lcd_puts("GPS Logger Project");
+    lcd_gotoxy(0,1);
     lcd_puts("By Hidayat and Manu");
-    lcd_gotoxy(0,2);	// Go to 3rd row 1st column
-    lcd_puts("CSE2 Projekt, CE");
+    lcd_gotoxy(0,2);
+    lcd_puts("Comp Eng HTW Berlin");
+    _delay_ms(4500);
+	
+    lcd_clrscr();
+    lcd_home();
+    lcd_puts("Reading from SD.");
+    _delay_ms(1000);
+    lcd_home();
+    lcd_puts("Reading from SD..");
+    _delay_ms(1000);
+    lcd_home();
+    lcd_puts("Reading from SD...");
+    _delay_ms(1000);
+    lcd_home();
+    lcd_puts("Reading from SD....");
+    _delay_ms(1000);
+	
+	lcd_clrscr();
+	lcd_home();
+    lcd_puts("Dest coor from SD");
+    lcd_gotoxy(0,1);
+    lcd_puts("Lat:");
+    lcd_gotoxy(0,2);
+    lcd_puts("Lon:");
 
-    for (i=0; i<10; i++){
-        error = SD_init();
-        if(!error)
-            break;
+    for(i=7,cur_pos_lat=4;i<17;i++){
+    lcd_gotoxy(cur_pos_lat,1);
+    lcd_putc(sdbuff[i]);
+    cur_pos_lat++;
+    }
+    for(j=18,cur_pos_lon=4;j<28;j++){
+	    lcd_gotoxy(cur_pos_lon,2);
+	    lcd_putc(sdbuff[j]);
+	    cur_pos_lon++;
     }
 
-    if(error){
-        _delay_ms(500);		// Delay before clearing the screen again
-        lcd_clrscr();
-        lcd_home();
-        if(error == 1) lcd_puts("No SD Card detected");
-        if(error == 2) lcd_puts("Card init failed");
-    }
+    _delay_ms(4000);
 
-    error = getBootSectorData (); //read boot sector of SD and keep necessary data in global variables
+    lcd_clrscr();
+    lcd_home();
+    lcd_puts("Fetch GPS coor.");
+    _delay_ms(1000);
+    lcd_clrscr();
+    lcd_home();
+    lcd_puts("Fetch GPS coor..");
+    _delay_ms(1000);
+    lcd_clrscr();
+    lcd_home();
+    lcd_puts("Fetch GPS coor...");
+    _delay_ms(1000);
+    lcd_clrscr();
+    lcd_home();
+    lcd_puts("Fetch GPS coor....");
+    _delay_ms(1000);
 
-    if(error){
-    	lcd_gotoxy(0,1);
-    	lcd_puts ("FAT32 not found!");  //FAT32 incompatible drive
-        _delay_ms(500);
-    }
-    else{
-        lcd_clrscr();
-        lcd_home();
-        lcd_puts("Read from SD...");
-        _delay_ms(500);
-    }
-
-    SPI_HIGH_SPEED;   //SCK - 4 MHz as defined in spi.h
-
-    // Keep track of received messages
-    uint8_t messageCount = 0;
-
-    /* Read the initial value from SD card and store it in sdbuff[128] */
-    error = readFile (READ, "in");
-
-	if(error){
-		lcd_gotoxy(0,1);
-		lcd_puts("No init val from SD");
-		_delay_ms(100);
-	}
-	else{
-		lcd_home();
-		lcd_puts("Starting");
-		_delay_ms(2000);
-        lcd_clrscr();
-		lcd_gotoxy(0,1);
-		lcd_puts("Lat:");
-		lcd_gotoxy(0,2);
-		lcd_puts("Lon:");
-		lcd_gotoxy(0,3);
-		lcd_puts("Dis:");
-	}
-
-    while(1) {
+	while(1) {
         // We'll write the data only if it contains a valid position
-        if (gps_getNMEA(nmeabuff, 128) & GPS_NMEA_VALID) {
-
-        	int disp_loop, arr_loop, pos_loop, sd_loop;
+        if (gps_getNMEA(nmeabuff, 128) & GPS_NMEA_VALID) {		
+			lcd_clrscr();
+			lcd_home();
+			lcd_puts("Position");
+			lcd_gotoxy(0,1);
+			lcd_puts("Dis:");
+			lcd_gotoxy(0,2);
+			lcd_puts("Lat:");
+			lcd_gotoxy(0,3);
+			lcd_puts("Lon:");
+				
+        	int arr_loop, pos_loop;
 
             /* Calculating distance */
             getDistance(nmeabuff, sdbuff, pLatGPS, pLongGPS, pLatSD, pLongSD, pDistance);
 
-            /* Displaying data on the LCD */
-            for(disp_loop = 0; disp_loop<2; disp_loop++){
-            	if(disp_loop == 0){
-                    lcd_home();
-            		lcd_puts("Destination");
-            		for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
-            			lcd_gotoxy(pos_loop,1);
-            			while(!(pLatSD[arr_loop] == '\0')){
-            				lcd_putc(pLatSD[arr_loop]);
-            				pos_loop++;
-            			}
-            		}
-            		for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
-            		    lcd_gotoxy(pos_loop,2);
-            		    while(!(pLongSD[arr_loop] == '\0')){
-            		    	lcd_putc(pLongSD[arr_loop]);
-            		    	pos_loop++;
-            		    }
-            		}
-            		for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
-            			lcd_gotoxy(pos_loop,3);
-            			while(!(pDistance[arr_loop] == '\0')){
-            				lcd_putc(pDistance[arr_loop]);
-            				pos_loop++;
-            			}
-            		}
-            		_delay_ms(400);
-            	}
-            	else{
-            		lcd_home();
-            		lcd_puts("Position   ");
-            		for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
-						lcd_gotoxy(pos_loop,1);
-						while(!(pLatGPS[arr_loop] == '\0')){
-							lcd_putc(pLatGPS[arr_loop]);
-							pos_loop++;
-						}
-					}
-					for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
-						lcd_gotoxy(pos_loop,2);
-						while(!(pLongGPS[arr_loop] == '\0')){
-							lcd_putc(pLongGPS[arr_loop]);
-							pos_loop++;
-						}
-					}
-            	}
-            	_delay_ms(400);
+            /* Displaying data on LCD */
+    		for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
+				lcd_gotoxy(pos_loop,2);
+				while(!(pLatGPS[arr_loop] == '\0')){
+					lcd_putc(pLatGPS[arr_loop]);
+					pos_loop++;
+				}
+			}
+			for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
+				lcd_gotoxy(pos_loop,3);
+				while(!(pLongGPS[arr_loop] == '\0')){
+					lcd_putc(pLongGPS[arr_loop]);
+					pos_loop++;
+				}
+			}
+            for(arr_loop = 0, pos_loop = 4; arr_loop < COORDINATE_BUFFER_SIZE; arr_loop++){
+                lcd_gotoxy(pos_loop,1);
+                while(!(pDistance[arr_loop] == '\0')){
+                    lcd_putc(pDistance[arr_loop]);
+                    pos_loop++;
+                }
             }
+            _delay_ms(500);
 
             /* Logging the data on the SD card */
-            for(sd_loop=0; sd_loop < COORDINATE_BUFFER_SIZE; sd_loop++){
-                dataString[sd_loop] = pLatGPS[sd_loop];
-            }
-            dataString[i++] = ',';
-
-            for(i=0; i < COORDINATE_BUFFER_SIZE; i++){
-                dataString[sd_loop] = pLongGPS[sd_loop];
-            }
-            dataString[i++] = ',';
-
-            error = writeFile("OUT");
-
-            if(error){
-                lcd_home();
-                lcd_puts("Error writing output");
-            }
-
-
+			if (f_open(&Fil, "OUT.txt", FA_WRITE | FA_CREATE_ALWAYS) == FR_OK) {
+				f_lseek(&Fil, write_pos);
+				f_write(&Fil, nmeabuff, 128, &bw);	/* Write data to the file */
+				write_pos += 128;
+				f_close(&Fil);
+						
+				if(bw == 128){
+					PORTC |= (1<<PORTC2);
+				}
+			}
         }
-        
+		else{
+			lcd_gotoxy(0,1);
+			lcd_puts("Waiting for valid");
+			lcd_gotoxy(0,2);
+			lcd_puts("coor from GPS...");
+		}
         sleep_mode();
     }
 
-    return 0;   /* never reached */
+	    return 0;   /* never reached */
 }
+
+
+/*---------------------------------------------------------*/
+/* User Provided RTC Function called by FatFs module       */
+
+DWORD get_fattime (void)
+{
+	/* Returns current time packed into a DWORD variable */
+	return	  ((DWORD)(2013 - 1980) << 25)	/* Year 2013 */
+	| ((DWORD)7 << 21)				/* Month 7 */
+	| ((DWORD)28 << 16)				/* Mday 28 */
+	| ((DWORD)0 << 11)				/* Hour 0 */
+	| ((DWORD)0 << 5)				/* Min 0 */
+	| ((DWORD)0 >> 1);				/* Sec 0 */
+}
+
